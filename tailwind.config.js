588128bb51f120c4b337ce3fdf4f2e7d09eebee7
Rojen Maharjan <44132059+rojen11@users.noreply.github.com>
2023-06-20 15:58:31 +0545

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,13 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+
       },
+      colors: {
+        "primary": "#DB6332",
+        "secondary": "#151E3F",
+        "white": "#F2F3D9"
+      }
     },
   },
   plugins: [],
