daef3cff3f18bdcaa602e0651502aa4289029f18
Rojen Maharjan <44132059+rojen11@users.noreply.github.com>
2023-07-16 12:31:09 +0545

    fix: email, background, hover

--- src/app/components/ContactUs-Button/contactusButton.tsx
+++ src/app/components/ContactUs-Button/contactusButton.tsx
@@ -5,7 +5,7 @@ const contactusButton = () => {
   return (
     <Link
       href={"/contact-us"}
-      className="bg-transparent hover:bg-beige text-beige font-semibold hover:text-black py-1 px-4 md:py-2 md:px-8 border border-beige hover:border-transparent rounded transition ease-out delay-300  "
+      className="bg-transparent hover:bg-beige text-beige font-semibold hover:text-black py-1 px-4 md:py-2 md:px-8 border border-beige hover:border-transparent rounded transition ease-out delay-75"
     >
       Book a Meeting
     </Link>
